f3bda3925f0dea2cef317c05e5bb6158aa647324
LEGIABAO-24102002 <legiabaoaabbcc@gmail.com>
2023-09-28 10:38:40 +0700

    chinh sua font chu

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import {  } from "module";
 
 export default function App() {
   return <AppNavigator />;
+  
 };
