08b6bbe4f5f4048c1d26f667fd8e0b2350514efe
LEGIABAO-24102002 <legiabaoaabbcc@gmail.com>
2023-10-04 10:44:21 +0700

    Tao data user & chinh sua mot so thu

---
+++ datauser.js
@@ -0,0 +1,29 @@
+const User = [
+  {
+    id: 1,
+    name: "huo",
+    email: "abc@gmail.com",
+    title: "Good ",
+    pass: "123",
+    imgUser:"https://cdn.glitch.global/a2f08375-f202-47c4-8950-a18cdb1db74f/unnamed%20(2).png",
+    date: "11/11/2011",
+    sothich: "Ngủ",
+    yeuthich: "Chuối",
+  },
+  {
+    id: 2,
+    name: "Duyên",
+    email: "abcd",
+    title: "Good ",
+    pass: "1",
+  },
+  {
+    id: 2,
+    name: "abc",
+    email: "abao ",
+    title: "Good ",
+    pass: "123"
+  }
+];
+
+export default User;
